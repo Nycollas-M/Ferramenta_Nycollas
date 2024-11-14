d978672d8d65826e129e41429232fe73d0a77442
Nycollas-M <00001122973184sp@al.educacao.sp.gov.br>
2024-11-14 10:11:00 -0300

    Add files via upload

---
+++ Ferramenta de estudo/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Inglaterra?',
+    'A capital da Inglaterra é Londres'
+)
+
+criaCartao(
+    'Mecânica',
+    'O que é um v6',
+    'Um v6 é um motor com pistôes em v contendo 6 pistôes'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz legal em Inglês?',
+    'Legal em ingles é cool (cul)'
+)
